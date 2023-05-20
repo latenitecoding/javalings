31811060c43b34b809b1af201a4417ff4e00dc8b
latenitecoding <latenitecode@gmail.com>
2023-05-20 18:14:45 -0500

    adds introduction

---
+++ exercises/Intro001.java
@@ -0,0 +1,38 @@
+// Intro001.java
+// In these exercises, we're going to learn/refresh a wide range of Java concepts from the most
+// basic to some of the most advanced. Please take time to review each lesson to ensure that you
+// "know" how to complete the exercise without the assistance of LSP or IDE tools.
+//
+// At the top of each file, you'll find a `I AM NOT DONE`. When you are finished completing
+// and reviewing the exercise, delete this comment. After the exercise has been successfully
+// completed and this comment has been deleted, you may move on to the next exercise.
+//
+// Execute `javalings hint Intro001` or use the `hint` watch-subcommand for a hint. If you're
+// running this using `javalings watch`: The exercise file will be reloaded when you change one of
+// the lines below! Try adding a `System.out.println` line, or try changing what it outputs in your
+// terminal. Try removing a semicolon and see what happens!
+
+// TODO: I AM NOT DONE
+
+public class Intro001 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello and");
+        System.out.println("       welcome to...                         ");
+        System.out.println("    _                   _ _                  ");
+        System.out.println("   (_) __ _ _   _  __ _| (_)_ __   __ _ ___  ");
+        System.out.println("   | |/ _` | | | |/ _` | | | '_ \\ / _` / __| ");
+        System.out.println("   | | (_| | |_| | (_| | | | | | | (_| \\__ \\ ");
+        System.out.println(" __| |\\__,_|\\___/ \\__,_|_|_|_| |_|\\__, |___/ ");
+        System.out.println("|___/                             |___/      ");
+        System.out.println();
+        System.out.println("This exercise compiles successfully. The remaining exercises contain");
+        System.out.println("a compiler or logic error. The central concept behind Javalings is to");
+        System.out.println("fix these errors and solve the exercises. Good luck!");
+        System.out.println();
+        System.out.println("The source for this exercise is in `exercises/Intro001.java`.");
+        System.out.println("Have a look! Going forward, exercises will always use a");
+        System.out.println("success/failure output.");
+        System.out.println();
+    }
+}
