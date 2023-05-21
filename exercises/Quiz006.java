74d2a82f415cf79d3ef5200a42b4af7f85229e1d
latenitecoding <latenitecode@gmail.com>
2023-05-21 09:39:00 -0500

    adds first quiz on hello world

---
+++ exercises/Quiz006.java
@@ -0,0 +1,4 @@
+// Write your first Java program to greet everyone with a `Hello, World!`
+// Execute `javalings hint Quiz006` or use the `hint` watch-subcommand for a hint.
+
+// TODO: I AM NOT DONE
