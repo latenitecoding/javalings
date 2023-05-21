9ecac6813cfc76686a578ed685b53a4c5259be1c
latenitecoding <latenitecode@gmail.com>
2023-05-21 09:36:53 -0500

    adds intro exercises

---
+++ exercises/Intro002.java
@@ -0,0 +1,46 @@
+// As you may know, Java is one of the few legacy programming languages still in popular demand
+// today. It was first released in 1995, and, at the time of this exercise, was one of the top
+// five most desired programming languages alongside Python3, SQL, JavaScript, and C/C++.
+// 
+// What distinguishes Java from other programming languages is its commitment to the Object
+// Oriented Programming (OOP) paradigm. Languages with a similar focus would include Smalltalk,
+// Eiffel, Scala, Ruby, and Crystal. Some of the other most desired programming languages 
+// mentioned earlier (Python3, JavaScript, C++) permit OOP but are designed around procedural
+// programming. Many more modern languages, such as Rust, Go, TypeScript, Julia, Kotlin, and Dart,
+// take a similar approach. You might hear these languages referred to as "general purpose" or
+// "multi-paradigmatic" languages. Generally speaking (pun intended) these languages are "better"
+// for programming since they permit developers to employ tools from many different programming
+// paradigms. The choice of whether to choose a more specialized language or a more general
+// language is much like the difference between choosing to be an expert in one field versus
+// knowledgeable in several fields. There are advantages and disadvantages to both, and the
+// weighing of those is heavily dependent on one's situation and goals.
+//
+// As it stands, OOP is currently the most widely used programming paradigm and, despite current
+// sentiment against OOP, it is still very useful for developers to understand this paradigm well.
+// Java remains one of the more useful languages to learn the OOP paradigm and even if you don't
+// intend to program in Java later on, you should still make an intentional effort to learn how you
+// might make better use of the OOP paradigm in your future projects.
+//
+// Even though Java remains a great language for teaching OOP, I feel obligated to stress that
+// OOP in Java is still done the "Java way". As we work through these exercises, you should try to
+// be conscientious of those aspects of the language that relate to the OOP paradigm and those
+// aspects that are just Java. To that end, I've included this long ramble at the beginning of
+// our first real Javalings exercise to not only teach you something about Java but also to help
+// you experience one of Java's first and most notable aspects: it's verbosity and boilerplate.
+//
+// To begin, all Java **files** must contain a single public outer class designated by the
+// keywords `public class` that shares the same name as the file itself. For example, the first
+// exercise `Intro001.java` contained the outer class `public class Intro001`.
+//
+// The following Java code won't compile. Fix this program's errors so that it can print a classic
+// greeting to the world.
+// Execute `javalings hint Intro002` or use the `hint` watch-subcommand for a hint.
+
+// TODO: I AM NOT DONE
+
+??? ??? ??? {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ exercises/Intro003.java
@@ -0,0 +1,21 @@
+// In addition, all Java programs should have a designated entry point. Java programs are run
+// within a special application called the Java Virtual Machine (JVM). This JVM runs as an
+// application under the purview of the OS and can execute Java code. When a Java program is
+// executed by the JVM, the JVM looks for the signature method
+//      `public static void main(String[] args)`
+// and calls it. Since this is the first method called by the JVM, it is our "entry point". Java
+// programs don't require an entry point, but without one, your application won't do anything when
+// executed. This is common for Java libraries but incorrect for tools and applications.
+//
+// The following Java code won't compile. Fix this program's errors so that it can print a classic
+// greeting to the world.
+// Execute `javalings hint Intro003` or use the `hint` watch-subcommand for a hint.
+
+// TODO: I AM NOT DONE
+
+public class Intro003 {
+
+    ??? ??? ??? ???(???[] ???) {
+        System.out.println("Hello, World!");
+    }
+}
