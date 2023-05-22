dce182683edabcf9521fcd7d9fc19cb04699fe1d
latenitecoding <latenitecode@gmail.com>
2023-05-21 22:09:27 -0500

    formats output of javalings list

--- src/main/java/com/latenitecode/javalings/Javalings.java
+++ src/main/java/com/latenitecode/javalings/Javalings.java
@@ -4,25 +4,67 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Javalings {
 
-    public static String list() {
-        HashMap<String, String> exercisesMap = new HashMap<>(100);
+    private static TreeMap<String, String> getExercises() {
+        TreeMap<String, String> exercises = new TreeMap<>();
         Stream.of(new File("exercises").listFiles())
             .filter(file -> !file.isDirectory())
             .map(File::getPath)
             .filter(path -> path.contains(".java"))
             .forEach(path -> {
-                exercisesMap.put(path.substring(path.length() - 8, path.length() - 5), path);
+                exercises.put(path.substring(path.length() - 8, path.length() - 5), path);
             });
-        ArrayList<String> sortedKeys = new ArrayList<>(exercisesMap.keySet());
-        Collections.sort(sortedKeys);
-        return sortedKeys
-            .stream()
-            .map(key -> exercisesMap.get(key))
-            .collect(Collectors.joining("\n"));
+        return exercises;
+    }
+
+    public static String list() {
+        return Javalings.list(true, true, true, 'a');
+    }
+
+    public static String list(boolean useName, boolean usePath, boolean useStatus, char status) {
+        TreeMap<String, String> exercises = Javalings.getExercises();
+        StringBuilder output = new StringBuilder();
+        if (useName) {
+            output.append(String.format("%-22s", "Name"));
+        }
+        if (usePath) {
+            output.append(String.format("%-44s", "Path"));
+        }
+        if (useStatus) {
+            output.append(String.format("%-8s", "Status"));
+        }
+        output.append("\n");
+        output.append(
+                exercises
+                    .keySet()
+                    .stream()
+                    .map(key -> {
+                        String path = exercises.get(key);
+                        StringBuilder builder = new StringBuilder();
+                        if (useName) {
+                            String name = path
+                                .substring(
+                                    path.lastIndexOf('/') + 1,
+                                    path.lastIndexOf('.')
+                                );
+                            builder.append(String.format("%-22s", name));
+                        }
+                        if (usePath) {
+                            builder.append(String.format("%-44s", path));
+                        }
+                        if (useStatus) {
+                            // TODO: check actual status
+                            builder.append("Not Done");
+                        }
+                        return builder.toString();
+                    })
+                    .collect(Collectors.joining("\n"))
+            );
+        return output.toString();
     }
 }
