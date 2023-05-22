84b0593ee76a23cfa762fad0c4d5ed5fa552cf55
latenitecoding <latenitecode@gmail.com>
2023-05-22 08:22:03 -0500

    reformats src code

--- src/main/java/com/latenitecode/javalings/Args.java
+++ src/main/java/com/latenitecode/javalings/Args.java
@@ -6,189 +6,189 @@ import java.util.Set;
 /** Javalings CLI tool */
 public class Args {
 
-    private static void assertNoCommand(Command command) {
-        if (command != Command.None) {
-            throw new RuntimeException("Cannot have two commands");
-        }
+  private static void assertNoCommand(Command command) {
+    if (command != Command.None) {
+      throw new RuntimeException("Cannot have two commands");
     }
+  }
 
-    private static void assertNoOption(Option option) {
-        if (option != Option.None) {
-            throw new RuntimeException("Cannot have two options");
-        }
+  private static void assertNoOption(Option option) {
+    if (option != Option.None) {
+      throw new RuntimeException("Cannot have two options");
     }
+  }
 
-    private static String formatCommand(String command, String desc) {
-        return String.format("  %-20s%s", command, desc);
+  private static String formatCommand(String command, String desc) {
+    return String.format("  %-20s%s", command, desc);
+  }
+
+  private static String formatOption(String option, String desc) {
+    return String.format(
+        "  %-20s%s",
+        String.format("-%s, --%s", option.charAt(0), option),
+        desc
+      );
+  }
+
+  public static Args parse(String version, String[] args) {
+    Command command = Command.None;
+    Option option = Option.None;
+    Set<String> clargs = new HashSet<>();
+    for (String arg : args) {
+      if (arg.equals("list")) {
+        assertNoCommand(command);
+        command = Command.List;
+        continue;
+      }
+      if (arg.equals("run")) {
+        assertNoCommand(command);
+        command = Command.Run;
+        continue;
+      }
+      if (arg.equals("verify")) {
+        assertNoCommand(command);
+        command = Command.Verify;
+        continue;
+      }
+      if (arg.equals("-h") || arg.equals("-help") || arg.equals("--help")) {
+        assertNoOption(option);
+        option = Option.Help;
+        continue;
+      }
+      if (arg.equals("-v") || arg.equals("--version")) {
+        assertNoOption(option);
+        option = Option.Version;
+        continue;
+      }
+      clargs.add(arg);
     }
+    return new Args(version, command, option, clargs);
+  }
 
-    private static String formatOption(String option, String desc) {
-        return String.format(
-                "  %-20s%s",
-                String.format("-%s, --%s", option.charAt(0), option),
-                desc
-            );
+  private Set<String> args;
+  private Command command;
+  private Option option;
+  private String version;
+
+  private Args(String version, Command command, Option option, Set<String> args) {
+    this.version = version;
+    this.command = command;
+    this.option = option;
+    this.args = args;
+  }
+
+  public Set<String> getArgs() {
+    return this.args;
+  }
+
+  public String getVersion() {
+    return this.version;
+  }
+
+  public boolean empty() {
+    return this.command == Command.None && this.option == Option.None;
+  }
+
+  public boolean hasArg(String arg) {
+    return this.args.contains(arg);
+  }
+
+  public boolean hasLongOrShortArg(String arg) {
+    return this.hasArg(String.format("--%s", arg)) ||
+      this.hasArg(String.format("-%s", arg.charAt(0)));
+  }
+
+  public boolean help() {
+    return this.option == Option.Help;
+  }
+
+  public boolean list() {
+    return this.command == Command.List;
+  }
+
+  public String toString() {
+    if (this.command == Command.List) {
+      return String.join(
+          "\n",
+          "Usage: javalings list [-p] [-n] [-u] [-s]",
+          "",
+          "Lists the exercises available in Javalings",
+          "",
+          "Options:",
+          Args.formatOption("paths", "show only the paths of the exercises"),
+          Args.formatOption("names", "show only the names of the exercises"),
+          Args.formatOption("unsolved", "display only exercises not yet solved"),
+          Args.formatOption("solved", "display only exercises that have been solved"),
+          Args.formatOption("help", "display usage information")
+        );
     }
-
-    public static Args parse(String version, String[] args) {
-        Command command = Command.None;
-        Option option = Option.None;
-        Set<String> clargs = new HashSet<>();
-        for (String arg : args) {
-            if (arg.equals("list")) {
-                assertNoCommand(command);
-                command = Command.List;
-                continue;
-            }
-            if (arg.equals("run")) {
-                assertNoCommand(command);
-                command = Command.Run;
-                continue;
-            }
-            if (arg.equals("verify")) {
-                assertNoCommand(command);
-                command = Command.Verify;
-                continue;
-            }
-            if (arg.equals("-h") || arg.equals("-help") || arg.equals("--help")) {
-                assertNoOption(option);
-                option = Option.Help;
-                continue;
-            }
-            if (arg.equals("-v") || arg.equals("--version")) {
-                assertNoOption(option);
-                option = Option.Version;
-                continue;
-            }
-            clargs.add(arg);
-        }
-        return new Args(version, command, option, clargs);
+    if (this.command == Command.Run) {
+      return String.join(
+          "\n",
+          "Usage: javalings run <name>",
+          "",
+          "Runs/Tests a single exercise",
+          "",
+          "Positional arguments:",
+          Args.formatCommand("name", "the name of the exercise"),
+          "",
+          "Options:",
+          Args.formatOption("help", "display usage information")
+        );
     }
-
-    private Set<String> args;
-    private Command command;
-    private Option option;
-    private String version;
-
-    private Args(String version, Command command, Option option, Set<String> args) {
-        this.version = version;
-        this.command = command;
-        this.option = option;
-        this.args = args;
+    if (this.command == Command.Verify) {
+      return String.join(
+          "\n",
+          "Usage: javalings verify",
+          "",
+          "Verifies all exercises according to the recommended order",
+          "",
+          "Options:",
+          Args.formatOption("help", "display usage information")
+        );
     }
+    return String.join(
+        "\n",
+        "Usage: javalings [-v] [<command>] [<args>]",
+        "",
+        "Javalings is a collection of small exercises to get you used to writing and reading Java code",
+        "",
+        "Options:",
+        Args.formatOption("version", "show the executable version"),
+        Args.formatOption("help", "display usage information"),
+        "",
+        "Commands:",
+        Args.formatCommand(
+          "verify",
+          "Verifies all exercises according to the recommended order"
+        ),
+        Args.formatCommand("run", "Runs/Tests a single exercise"),
+        Args.formatCommand("list", "Lists the exercises available in Javalings")
+      );
+  }
 
-    public Set<String> getArgs() {
-        return this.args;
-    }
+  public boolean run() {
+    return this.command == Command.Run;
+  }
 
-    public String getVersion() {
-        return this.version;
-    }
+  public boolean verify() {
+    return this.command == Command.Verify;
+  }
 
-    public boolean empty() {
-        return this.command == Command.None && this.option == Option.None;
-    }
+  public boolean version() {
+    return this.option == Option.Version;
+  }
 
-    public boolean hasArg(String arg) {
-        return this.args.contains(arg);
-    }
+  public static enum Command {
+    None,
+    List,
+    Run,
+    Verify
+  }
 
-    public boolean hasLongOrShortArg(String arg) {
-        return this.hasArg(String.format("--%s", arg)) ||
-            this.hasArg(String.format("-%s", arg.charAt(0)));
-    }
-
-    public boolean help() {
-        return this.option == Option.Help;
-    }
-
-    public boolean list() {
-        return this.command == Command.List;
-    }
-
-    public String toString() {
-        if (this.command == Command.List) {
-            return String.join(
-                    "\n",
-                    "Usage: javalings list [-p] [-n] [-u] [-s]",
-                    "",
-                    "Lists the exercises available in Javalings",
-                    "",
-                    "Options:",
-                    Args.formatOption("paths", "show only the paths of the exercises"),
-                    Args.formatOption("names", "show only the names of the exercises"),
-                    Args.formatOption("unsolved", "display only exercises not yet solved"),
-                    Args.formatOption("solved", "display only exercises that have been solved"),
-                    Args.formatOption("help", "display usage information")
-                );
-        }
-        if (this.command == Command.Run) {
-            return String.join(
-                    "\n",
-                    "Usage: javalings run <name>",
-                    "",
-                    "Runs/Tests a single exercise",
-                    "",
-                    "Positional arguments:",
-                    Args.formatCommand("name", "the name of the exercise"),
-                    "",
-                    "Options:",
-                    Args.formatOption("help", "display usage information")
-                );
-        }
-        if (this.command == Command.Verify) {
-            return String.join(
-                    "\n",
-                    "Usage: javalings verify",
-                    "",
-                    "Verifies all exercises according to the recommended order",
-                    "",
-                    "Options:",
-                    Args.formatOption("help", "display usage information")
-                );
-        }
-        return String.join(
-                "\n",
-                "Usage: javalings [-v] [<command>] [<args>]",
-                "",
-                "Javalings is a collection of small exercises to get you used to writing and reading Java code",
-                "",
-                "Options:",
-                Args.formatOption("version", "show the executable version"),
-                Args.formatOption("help", "display usage information"),
-                "",
-                "Commands:",
-                Args.formatCommand(
-                    "verify",
-                    "Verifies all exercises according to the recommended order"
-                ),
-                Args.formatCommand("run", "Runs/Tests a single exercise"),
-                Args.formatCommand("list", "Lists the exercises available in Javalings")
-            );
-    }
-
-    public boolean run() {
-        return this.command == Command.Run;
-    }
-
-    public boolean verify() {
-        return this.command == Command.Verify;
-    }
-
-    public boolean version() {
-        return this.option == Option.Version;
-    }
-
-    public static enum Command {
-        None,
-        List,
-        Run,
-        Verify
-    }
-
-    public static enum Option {
-        Help,
-        None,
-        Version
-    }
+  public static enum Option {
+    Help,
+    None,
+    Version
+  }
 }
--- src/main/java/com/latenitecode/javalings/Javalings.java
+++ src/main/java/com/latenitecode/javalings/Javalings.java
@@ -16,203 +16,197 @@ import java.util.stream.Stream;
 
 public class Javalings {
 
-    private static TreeMap<String, String> getExercises() {
-        TreeMap<String, String> exercises = new TreeMap<>();
-        Stream
-            .of(new File("exercises").listFiles())
-            .filter(file -> !file.isDirectory())
-            .map(File::getPath)
-            .filter(path -> path.contains(".java"))
-            .forEach(path -> {
-                exercises.put(path.substring(path.length() - 8, path.length() - 5), path);
-            });
-        return exercises;
+  private static TreeMap<String, String> getExercises() {
+    TreeMap<String, String> exercises = new TreeMap<>();
+    Stream
+      .of(new File("exercises").listFiles())
+      .filter(file -> !file.isDirectory())
+      .map(File::getPath)
+      .filter(path -> path.contains(".java"))
+      .forEach(path -> {
+        exercises.put(path.substring(path.length() - 8, path.length() - 5), path);
+      });
+    return exercises;
+  }
+
+  private static TreeSet<String> getProgress() {
+    try {
+      return Files
+        .lines(Path.of(".progress"))
+        .collect(Collectors.toCollection(TreeSet::new));
+    } catch (IOException e) {
+      System.err.println(e);
+      return new TreeSet<>();
     }
+  }
 
-    private static TreeSet<String> getProgress() {
-        try {
-            return Files
-                .lines(Path.of(".progress"))
-                .collect(Collectors.toCollection(TreeSet::new));
-        } catch (IOException e) {
-            System.err.println(e);
-            return new TreeSet<>();
-        }
+  private static void report(int passing, int total) {
+    StringBuilder progress = new StringBuilder();
+    if (passing > 0) {
+      progress.append("\033[0;32m");
     }
-
-    private static void report(int passing, int total) {
-        StringBuilder progress = new StringBuilder();
-        if (passing > 0) {
-            progress.append("\033[0;32m");
-        }
-        for (int i = 0; i < passing - 1; i++) {
-            progress.append('#');
-        }
-        if (passing > 0) {
-            progress.append('>');
-            progress.append("\033[0m");
-        }
-        if (passing < total) {
-            progress.append("\033[0;31m");
-        }
-        for (int i = passing; i < total; i++) {
-            progress.append('-');
-        }
-        if (passing < total) {
-            progress.append("\033[0m");
-        }
-        System.out.printf(
-                "Progress: [%s] %d/%d (%.1f %%)\n",
-                progress.toString(),
-                passing,
-                total,
-                (double) passing / total
-            );
+    for (int i = 0; i < passing - 1; i++) {
+      progress.append('#');
     }
-
-    public static Result list() {
-        return Javalings.list(true, true, 'a');
+    if (passing > 0) {
+      progress.append('>');
+      progress.append("\033[0m");
     }
+    if (passing < total) {
+      progress.append("\033[0;31m");
+    }
+    for (int i = passing; i < total; i++) {
+      progress.append('-');
+    }
+    if (passing < total) {
+      progress.append("\033[0m");
+    }
+    System.out.printf(
+        "Progress: [%s] %d/%d (%.1f %%)\n",
+        progress.toString(),
+        passing,
+        total,
+        (double) passing / total
+      );
+  }
 
-    public static Result list(boolean useName, boolean usePath, char status) {
-        StringBuilder output = new StringBuilder();
-        if (useName) {
-            output.append(String.format("%-22s", "Name"));
-        }
-        if (usePath) {
-            output.append(String.format("%-44s", "Path"));
-        }
-        output.append(String.format("%-8s", "Status"));
-        output.append("\n");
+  public static Result list() {
+    return Javalings.list(true, true, 'a');
+  }
 
-        TreeMap<String, String> exercises = Javalings.getExercises();
-        TreeSet<String> progress = Javalings.getProgress();
+  public static Result list(boolean useName, boolean usePath, char status) {
+    StringBuilder output = new StringBuilder();
+    if (useName) {
+      output.append(String.format("%-22s", "Name"));
+    }
+    if (usePath) {
+      output.append(String.format("%-44s", "Path"));
+    }
+    output.append(String.format("%-8s", "Status"));
+    output.append("\n");
+
+    TreeMap<String, String> exercises = Javalings.getExercises();
+    TreeSet<String> progress = Javalings.getProgress();
+    output.append(
+        exercises
+          .keySet()
+          .stream()
+          .map(key -> exercises.get(key))
+          .filter(path -> {
+            return status == 'a' || status == 'u' && !progress.contains(path)
+              || status == 's' && progress.contains(path);
+          })
+          .map(path -> {
+            StringBuilder builder = new StringBuilder();
+            if (useName) {
+              String name = path
+                .substring(
+                  path.lastIndexOf('/') + 1,
+                  path.lastIndexOf('.')
+                );
+              builder.append(String.format("%-22s", name));
+            }
+            if (usePath) {
+              builder.append(String.format("%-44s", path));
+            }
+            builder.append(String.format("%-8s", (progress.contains(path)) ? "Done" : "Not Done"));
+            return builder.toString();
+          })
+          .collect(Collectors.joining("\n"))
+      );
+    return new Result(true, output.toString());
+  }
+
+  public static Result run(String name) {
+    try {
+      boolean isWindows = System.getProperty("os.name").toLowerCase().startsWith("windows");
+      String javac = String.format("javac -d target/exercises exercises/%s.java", name);
+      String java = String.format("java -cp target/exercises %s", name);
+
+      ProcessBuilder builder = new ProcessBuilder();
+      if (isWindows) {
+        builder.command("cmd.exe", "/c", String.format("%s ; %s", javac, java));
+      } else {
+        builder.command("bash", "-c", String.format("%s ; %s", javac, java));
+      }
+
+      Process proc = builder.start();
+      int exitCode = proc.waitFor();
+      StringBuilder output = new StringBuilder();
+      BufferedReader reader;
+
+      if (exitCode != 0) {
+        reader = new BufferedReader(new InputStreamReader(proc.getErrorStream()));
+        output.append(reader.lines().collect(Collectors.joining("\n")));
+        output.append("\n\n\033[0;31m");
         output.append(
-                exercises
-                    .keySet()
-                    .stream()
-                    .map(key -> exercises.get(key))
-                    .filter(path -> {
-                        return status == 'a' || status == 'u' && !progress.contains(path)
-                            || status == 's' && progress.contains(path);
-                    })
-                    .map(path -> {
-                        StringBuilder builder = new StringBuilder();
-                        if (useName) {
-                            String name = path
-                                .substring(
-                                    path.lastIndexOf('/') + 1,
-                                    path.lastIndexOf('.')
-                                );
-                            builder.append(String.format("%-22s", name));
-                        }
-                        if (usePath) {
-                            builder.append(String.format("%-44s", path));
-                        }
-                        builder
-                            .append(
-                                String.format(
-                                    "%-8s",
-                                    (progress.contains(path)) ? "Done" : "Not Done"
-                                )
-                            );
-                        return builder.toString();
-                    })
-                    .collect(Collectors.joining("\n"))
-            );
-        return new Result(true, output.toString());
+            String.format(
+              "\u26A0 Testing of exercises/%s.java failed! Please try again.", 
+              name
+            )
+          );
+        output.append("\033[0m");
+        reader.close();
+        return new Result(false, output.toString());
+      }
+
+      reader = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+      output.append(reader.lines().collect(Collectors.joining("\n")));
+      output.append("\n\n\033[0;32m");
+      output.append(String.format("\u2705 Successfully ran exercises/%s.java", name));
+      output.append("\033[0m");
+      reader.close();
+      return new Result(true, output.toString());
+    } catch (InterruptedException | IOException e) {
+      System.err.println(e);
+      return new Result(false, String.format("Unable to run %s", name));
+    }
+  }
+
+  public static Result verify(boolean shouldReport) {
+    try {
+      BufferedWriter writer = new BufferedWriter(new FileWriter(".progress"));
+      Collection<String> exercises = Javalings.getExercises().values();
+      int passCount = 0;
+      for (String exercise : exercises) {
+        Result run = Javalings.run(
+            exercise.substring(exercise.lastIndexOf("/") + 1, exercise.lastIndexOf("."))
+          );
+        if (!run.ok()) {
+          writer.close();
+          return run;
+        }
+        passCount++;
+        writer.write(exercise);
+        writer.flush();
+        if (shouldReport) {
+          Javalings.report(passCount, exercises.size());
+        }
+      }
+      writer.close();
+      return new Result(true, "Congratulations!");
+    } catch (IOException e) {
+      System.err.println(e);
+      return new Result(false, "Unable to verify exercises");
+    }
+  }
+
+  public static class Result {
+    
+    private String msg;
+    private boolean ok;
+
+    public Result(boolean ok, String msg) {
+      this.ok = ok;
+      this.msg = msg;
     }
 
-    public static Result run(String name) {
-        try {
-            boolean isWindows = System.getProperty("os.name").toLowerCase().startsWith("windows");
-            String javac = String.format("javac -d target/exercises exercises/%s.java", name);
-            String java = String.format("java -cp target/exercises %s", name);
-
-            ProcessBuilder builder = new ProcessBuilder();
-            if (isWindows) {
-                builder.command("cmd.exe", "/c", String.format("%s ; %s", javac, java));
-            } else {
-                builder.command("bash", "-c", String.format("%s ; %s", javac, java));
-            }
-
-            Process proc = builder.start();
-            int exitCode = proc.waitFor();
-            StringBuilder output = new StringBuilder();
-            BufferedReader reader;
-
-            if (exitCode != 0) {
-                reader = new BufferedReader(new InputStreamReader(proc.getErrorStream()));
-                output.append(reader.lines().collect(Collectors.joining("\n")));
-                output.append("\n\n\033[0;31m");
-                output.append(
-                        String.format(
-                            "\u26A0 Testing of exercises/%s.java failed! Please try again.", 
-                            name
-                        )
-                    );
-                output.append("\033[0m");
-                reader.close();
-                return new Result(false, output.toString());
-            }
-
-            reader = new BufferedReader(new InputStreamReader(proc.getInputStream()));
-            output.append(reader.lines().collect(Collectors.joining("\n")));
-            output.append("\n\n\033[0;32m");
-            output.append(String.format("\u2705 Successfully ran exercises/%s.java", name));
-            output.append("\033[0m");
-            reader.close();
-            return new Result(true, output.toString());
-        } catch (InterruptedException | IOException e) {
-            System.err.println(e);
-            return new Result(false, String.format("Unable to run %s", name));
-        }
+    public boolean ok() {
+      return this.ok;
     }
 
-    public static Result verify(boolean shouldReport) {
-        try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter(".progress"));
-            Collection<String> exercises = Javalings.getExercises().values();
-            int passCount = 0;
-            for (String exercise : exercises) {
-                Result run = Javalings.run(
-                        exercise.substring(exercise.lastIndexOf("/") + 1, exercise.lastIndexOf("."))
-                    );
-                if (!run.ok()) {
-                    writer.close();
-                    return run;
-                }
-                passCount++;
-                writer.write(exercise);
-                writer.flush();
-                if (shouldReport) {
-                    Javalings.report(passCount, exercises.size());
-                }
-            }
-            writer.close();
-            return new Result(true, "Congratulations!");
-        } catch (IOException e) {
-            System.err.println(e);
-            return new Result(false, "Unable to verify exercises");
-        }
+    public String toString() {
+      return this.msg;
     }
-
-    public static class Result {
-        
-        private String msg;
-        private boolean ok;
-
-        public Result(boolean ok, String msg) {
-            this.ok = ok;
-            this.msg = msg;
-        }
-
-        public boolean ok() {
-            return this.ok;
-        }
-
-        public String toString() {
-            return this.msg;
-        }
-    }
+  }
 }
